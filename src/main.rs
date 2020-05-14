7d044e11ba829996ac51af49eddb46ed60e29175
RedTechKnight <bloodtechknight@protonmail.com>
2020-05-14 11:24:46 +0200

    Added basic constructors and selectors for Expression type. Also implemented functions to factor out negations and subtractions.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,17 @@ use std::collections::HashMap;
 use std::fmt;
 use std::hash::{Hash, Hasher};
 
-fn main() {}
+fn main() {
+    println!(
+        "{:?}",
+        Expression::binary_expression(
+            Operator::Sub,
+            Expression::real_expression(1.0),
+            Expression::integer_expression(230)
+        )
+        .factor_out_sub()
+    );
+}
 
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
 enum Literal {
@@ -92,7 +102,7 @@ impl Term {
         None
     }
 }
-
+#[derive(Debug, Clone, PartialOrd, PartialEq)]
 enum Operator {
     Paren,
     Neg,
@@ -101,12 +111,118 @@ enum Operator {
     Sub,
     Div,
     Exp,
-    Custom(String)
+    Custom(String),
 }
 
+#[derive(Debug, Clone, PartialOrd, PartialEq)]
 enum Expression {
     Lit(Term),
-    Unary(Operator,Box<Expression>),
-    Binary(Operator,Box<Expression>,Box<Expression>),
-    Variadic(Operator,Vec<Expression>)
+    Unary(Operator, Box<Expression>),
+    Binary(Operator, Box<Expression>, Box<Expression>),
+    Variadic(Operator, Vec<Expression>),
+}
+
+impl Expression {
+    fn variable_expression(a: char) -> Expression {
+        Expression::Lit(Term::variable_term(a))
+    }
+
+    fn real_expression(a: f64) -> Expression {
+        Expression::Lit(Term::real_term(a))
+    }
+
+    fn integer_expression(a: i128) -> Expression {
+        Expression::Lit(Term::integer_term(a))
+    }
+
+    fn unary_expression(f: Operator, a: Expression) -> Expression {
+        Expression::Unary(f, Box::new(a))
+    }
+
+    fn binary_expression(f: Operator, a: Expression, b: Expression) -> Expression {
+        Expression::Binary(f, Box::new(a), Box::new(b))
+    }
+
+    fn variadic_expression(f: Operator, exprs: Vec<Expression>) -> Expression {
+        Expression::Variadic(f, exprs)
+    }
+
+    fn get_literal(self) -> Option<Term> {
+        if let (Expression::Lit(a)) = self {
+            return Some(a);
+        }
+        None
+    }
+
+    fn get_unary_expression(self) -> Option<(Operator, Expression)> {
+        if let (Expression::Unary(f, a)) = self {
+            return Some((f, *a));
+        }
+        None
+    }
+
+    fn get_binary_expression(self) -> Option<(Operator, Expression, Expression)> {
+        if let (Expression::Binary(f, a, b)) = self {
+            return Some((f, *a, *b));
+        }
+        None
+    }
+
+    fn get_variadic_expression(self) -> Option<(Operator, Vec<Expression>)> {
+        if let Expression::Variadic(f, exprs) = self {
+            return Some((f, exprs));
+        }
+        None
+    }
+
+    fn factor_out_neg(self) -> Expression {
+        match self {
+            Expression::Unary(Operator::Neg, a) => {
+                return Expression::binary_expression(
+                    Operator::Mul,
+                    Expression::integer_expression(-1),
+                    a.factor_out_neg(),
+                );
+            }
+            l @ Expression::Lit(_) => l,
+            Expression::Unary(f, a) => Expression::unary_expression(f, a.factor_out_neg()),
+            Expression::Binary(f, a, b) => {
+                Expression::binary_expression(f, a.factor_out_neg(), b.factor_out_neg())
+            }
+            Expression::Variadic(f, exprs) => Expression::variadic_expression(
+                f,
+                exprs.into_iter().map(Expression::factor_out_neg).collect(),
+            ),
+        }
+    }
+
+    fn factor_out_sub(self) -> Expression {
+        match self {
+            Expression::Binary(Operator::Sub, a, b) => {
+                return Expression::binary_expression(
+                    Operator::Add,
+                    a.factor_out_sub(),
+                    Expression::binary_expression(
+                        Operator::Mul,
+                        Expression::integer_expression(-1),
+                        b.factor_out_sub(),
+                    ),
+                )
+            }
+            l @ Expression::Lit(_) => l,
+            Expression::Unary(f, a) => Expression::unary_expression(f, a.factor_out_sub()),
+            Expression::Binary(f, a, b) => {
+                Expression::binary_expression(f, a.factor_out_sub(), b.factor_out_sub())
+            }
+            Expression::Variadic(f, exprs) => Expression::variadic_expression(
+                f,
+                exprs
+                    .into_iter()
+                    .map(Expression::factor_out_sub)
+                    .collect::<Vec<Expression>>(),
+            ),
+        }
+    }
+
+    
 }
